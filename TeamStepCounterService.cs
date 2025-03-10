b4f518584a441d8dd1d95478e62a5fe681ea6201
Alexsid <10518162+alexsid-g@users.noreply.github.com>
2025-03-10 18:37:42 +0100

    Fix typo

--- TeamStepCounterService.cs
+++ TeamStepCounterService.cs
@@ -61,6 +61,6 @@ public class TeamStepCounterService
 
     public List<Team> ListTeams()
     {
-        return _teams.Select(x => new Team(x.Key, x.Value.Values.Sum(}).ToList();
+        return _teams.Select(x => new Team(x.Key, x.Value.Values.Sum()).ToList();
     }
 }
